1db9ab6a22b819acc0228645f17e004a824cf7d7
Danial Maleki <dmaleki@uwaterloo.ca>
2023-03-10 10:45:25 -0500

    init commit

---
+++ dynamic-stack.cpp
@@ -0,0 +1,39 @@
+#include "dynamic-stack.h"
+
+const DynamicStack::StackItem DynamicStack::EMPTY_STACK = -999;
+
+DynamicStack::DynamicStack() {
+
+}
+
+DynamicStack::DynamicStack(unsigned int capacity){
+
+}
+
+DynamicStack::~DynamicStack() {
+
+}
+
+unsigned int DynamicStack::size() const {
+
+}
+
+bool DynamicStack::empty() const {
+
+}
+
+DynamicStack::StackItem DynamicStack::peek() const {
+
+}
+
+void DynamicStack::push(StackItem value) {
+
+}
+
+DynamicStack::StackItem DynamicStack::pop() {
+
+}
+
+void DynamicStack::print() const {
+
+}
